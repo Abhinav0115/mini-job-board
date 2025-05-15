847e065d467c862cd3a0805d553b85dc74cf81c1
Abhinav Anand <abhinav011501@gmail.com>
2025-05-15 18:58:55 +0530

    update MongoDB connection fallback in server

--- server/server.js
+++ server/server.js
@@ -16,7 +16,9 @@ app.use("/api/jobs", jobRoutes);
 const PORT = process.env.PORT || 5000;
 
 mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(
+        process.env.MONGO_URI || "mongodb://localhost:27017/mini_job_board"
+    )
     .then(() => {
         app.listen(PORT, () => {
             console.log(`Server running on port ${PORT}`);
